6d3094037ecb9564f0e0c051c0c94a367c675529
Pond666 <suphakitpyto3321@gmail.com>
2021-11-16 17:25:27 +0700

    HELLO GUY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'NG101';
+  user = { id: 1001, name:"SUPHAKIT",address:"ENTITY303"}
+
+  i:number = 0;
+  nameInput:string="";
+  sayHi(){
+    return "Hi I Behind YOU!!!"
+  }
+
+  addCount(){
+    this.i++;
+  }
+  disCount(){
+    this.i--;
+  }
+
+  readName(name:string){
+    console.log(name);
+    this.nameInput=name;
+  }
+  
+  valid: boolean = 1<2;
+  validAge:boolean=false;
+
+  readAge(age:string){
+    let a = parseInt(age)
+    this.validAge = a>=18 && a<=25
+  }
+
+  menus = ["Shabu","KFC","MK","BBQ","Salad"];
+
+  courses =[
+    {id: 1,title:"TypeScript"},
+    {id: 2,title:"JAVA"},
+    {id: 3,title:"PHP"},
+    {id: 4,title:"Angular"},
+    {id: 5,title:"Flutter"}
+  ]
+
 }
